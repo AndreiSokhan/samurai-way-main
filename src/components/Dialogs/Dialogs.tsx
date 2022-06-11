ef57a9d325890c8766359d61e0475a6c90969762
Andrei <sokhanpageproofs@gmail.com>
2022-06-11 17:49:37 +0300

    samurai_way lesson_22 end

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
    return (
       <div className={s.Dialogs}>
-         Dialogs
+         {/*<h1>DIALOGS</h1>*/}
+         <div className={s.DialogsItems}>
+            <div className={s.Dialog}>
+               <NavLink to="/dialogs/1" activeClassName={s.active}>Dima</NavLink>
+            </div>
+            <div className={s.Dialog}>
+               <NavLink to="/dialogs/2" activeClassName={s.active}>Max</NavLink>
+            </div>
+            <div className={s.Dialog}>
+               <NavLink to="/dialogs/3" activeClassName={s.active}>Igor</NavLink>
+            </div>
+            <div className={s.Dialog}>
+               <NavLink to="/dialogs/4" activeClassName={s.active}>Lera</NavLink>
+            </div>
+            <div className={s.Dialog}>
+               <NavLink to="/dialogs/5" activeClassName={s.active}>Dasha</NavLink>
+            </div>
+         </div>
+         <div className={s.ChatItems}>
+            <div className={s.Chat}>Hi</div>
+            <div className={s.Chat}>How are you?</div>
+            <div className={s.Chat}>What are you doing?</div>
+            <div className={s.Chat}>What you thin about IT-KAMASUTRA?</div>
+         </div>
       </div>
    );
 };
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,11 +6,21 @@ export function NavBar() {
    return (
       <div className={s.NavBar}>
          <ul className={s.NavBarList}>
-            <li className={s.NavBarItem}><NavLink to="/profile" activeClassName={s.active}>Profile</NavLink></li>
-            <li className={s.NavBarItem}><NavLink to="/dialogs" activeClassName={s.active}>Message</NavLink></li>
-            <li className={s.NavBarItem}><NavLink to="/news" activeClassName={s.active}>News</NavLink></li>
-            <li className={s.NavBarItem}><NavLink to="/music" activeClassName={s.active}>Music</NavLink></li>
-            <li className={s.NavBarItem}><NavLink to="/settings" activeClassName={s.active}>Settings</NavLink></li>
+            <li className={s.NavBarItem}>
+               <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+            </li>
+            <li className={s.NavBarItem}>
+               <NavLink to="/dialogs" activeClassName={s.active}>Message</NavLink>
+            </li>
+            <li className={s.NavBarItem}>
+               <NavLink to="/news" activeClassName={s.active}>News</NavLink>
+            </li>
+            <li className={s.NavBarItem}>
+               <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
+            </li>
+            <li className={s.NavBarItem}>
+               <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+            </li>
          </ul>
       </div>
    );
