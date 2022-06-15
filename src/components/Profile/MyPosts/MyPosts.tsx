6146f2a9b34afda455299b8f87e29249f0aceebc
Andrei <sokhanpageproofs@gmail.com>
2022-06-15 23:29:43 +0300

    samurai_way lesson_27 in_progress

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,17 +2,23 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import {Post} from "./Post/Post";
 
-export const MyPosts = () => {
+export type PostPropsType = {
+   id: string
+   message: string
+   likesCount: number
+}
 
-   let postData = [
-      {id: 1, message: 'Hi, how are you', likesCount: 12},
-      {id: 2, message: "it's my first post in my social network", likesCount: 2},
-      {id: 2, message: "Hello WORLD", likesCount: 50},
-      {id: 2, message: "it's amazing", likesCount: 23},
-      {id: 2, message: "i'm a future it specialist", likesCount: 43}
-   ]
+type PostDataPropsType={
+   posts:Array<PostPropsType>
+}
 
-   let postEl = postData.map((p) => <Post message={p.message} likesCount={p.likesCount}/>)
+
+// tasks: Array<TasksPropsType>
+
+export const MyPosts = (props:PostDataPropsType) => {
+
+   let postEl =
+      props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount}/>)
 
    return (
       <div className={s.MyPosts}>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,13 +3,25 @@ import s from './Profile.module.css';
 import {MyPosts} from "./MyPosts/MyPosts";
 import {ProfileInfo} from "./ProfileInfo/ProfileInfo";
 
-export function Profile() {
+export const Profile = () => {
+
+   let posts = [
+      {id: 1, message: 'Hi, how are you', likesCount: 12},
+      {id: 2, message: "it's my first post in my social network", likesCount: 2},
+      {id: 2, message: "Hello WORLD", likesCount: 50},
+      {id: 2, message: "it's amazing", likesCount: 23},
+      {id: 2, message: "i'm a future it specialist", likesCount: 43}
+   ]
+
    return (
       <div>
          <div className={s.ProfileHeaderImg}>
             <ProfileInfo/>
-            <MyPosts/>
+            <MyPosts posts={}/>
          </div>
       </div>
    );
 }
+
+
+//время 28 урока на котором остановился - 4:44 мин
